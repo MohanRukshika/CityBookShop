fb7481ae3824177a01370639605cb472a4cd3120
Mohan Rukshika <mohanrukshika054@gmail.com>
2026-09-08 13:26:22 +0530

    Add files via upload

---
+++ model/Cashier.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Cashier extends User{
+    public Cashier(String username,String password){
+
+        super(username,password);
+    }
+
+    @Override
+    public String getRole() {
+        return "Cashier";
+    }
+}
